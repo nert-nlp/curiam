b5bb0be015ba980258dd78ed737b3852036fd528
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2023-06-17 03:32:30 -0400

    Refactored code and improved documentation

--- src/curiam/inception/tsv_processing.py
+++ src/curiam/inception/tsv_processing.py
@@ -1,84 +1,95 @@
 """Functions for processing the TSV-formatted data exported from Inception."""
 
-
 from typing import Tuple
 
 
-def process_compound_label(label):
-    """Splits a multi-part token label and returns the separate category labels
-    (and which annotation they belong to)."""
+def split_compound_label(compound_label: str) -> Tuple[list[str], list[str]]:
+    """Splits a compound token label into categories and token indexes.
 
-    sublabels = label.split("|")
+    Example: ["Example Use[56]|Direct Quote[58]"] returns
+    (["Example Use", "Direct Quote"], [56, 58]).
+
+    If an annotation isn't indexed, it only covers a single token and there's
+    only one annotation on that token. These are given these an annotation
+    index of -1.
+
+    Single-token spans on a token with multiple annotations do get indexed.
+
+    Args:
+        label: A pipe-separated string of annotations. A single annotation
+          is the category followed by its index in square brackets.
+
+    Returns:
+        A list of categories and a list of corresponding indexes.
+    """
+
+    sublabels = compound_label.split("|")
     categories = []
     annotation_indexes = []
     for sublabel in sublabels:
-        # If annotation isn't indexed , it only covers a single token and there's only one label
-        # Give these an annotation_index of -1.
-
-        # Spans that are only a single token that are part of some longer span still get indexed,
-        # like Focal Term[59] below.
-
-        """
-        62-32	8336-8337	"	*[56]|*[58]	Example Use[56]|Direct Quote[58]
-        62-33	8338-8342	same	*[56]|*[58]|*[59]	Example Use[56]|Direct Quote[58]|Focal Term[59]
-        62-34	8343-8344	"	*[56]|*[58]	Example Use[56]|Direct Quote[58]
-        """
-
-        # If there are two overlapping single-token annotations,
-        # the format is "Appeal to Meaning[1]|Metalinguistic Cue[2]"
         if "[" not in sublabel:
             categories.append(sublabel)
             annotation_indexes.append(-1)
         else:
-            bracket_index = sublabel.index("[")
-            categories.append(sublabel[:bracket_index])
-            annotation_indexes.append(int(sublabel[bracket_index+1:-1]))
+            # Remove the close bracket and then split on the open bracket
+            sublabel = sublabel.replace("]", "")
+            category, annotation_index = sublabel.split("[")
+            annotation_index = int(annotation_index)
+            categories.append(category)
+            annotation_indexes.append(annotation_index)
     return categories, annotation_indexes
 
 
-def process_sentence(tsv_rows: list) -> list:
-    """Parse TSV rows for a sentence into a list of simplified TSV rows.
+def process_sentence(tsv_rows: list) -> list[list]:
+    """Parses TSV rows for sentence tokens into a simpler format.
 
-    Each simplified row contains:
-    - Sentence number
-    - Token
-    - Label(s)
+    Reindexes annotations from document level to sentence level. Single-token
+    annotations (where that token only has 1 annotation)retain their -1 index.
 
-    The format of the labels is "{category}:{index}" and if a token has multiple
-    labels, they are separated by a pipe ("|").
+    Example: "Direct Quote[82]|Direct Quote[83]" is a label for a token that
+    is part of a nested direct quote. Assuming 82 is the first annotation
+    of the sentence, the resulting token label will will be:
 
-    Reindexes annotations from document level to sentence level.
+    {"categories": ["Direct Quote", "Direct Quote"],
+     "indexes": [1, 2]}
 
-    Example: "Direct Quote[82]|Direct Quote[83]" is a label for a token that is part of a nested direct quote.
-    Assuming 82 is the first annotation of the sentence, the result will be:
-        categories= ["Direct Quote", "Direct Quote"]
-        annotation_indexes = [1, 2]
+    Each token's labels are represented as a dictionary containing "categories"
+    and "indexes" which identify which categories the token has been annotated
+    with and which annotation it is part of in the sentence.
 
+    Args:
+        tsv_rows: a list of tab-separated strings read from INCEpTION's TSV
+          export format.
+
+    Returns:
+        A list of tokens represented as a list:
+          [sentence_number, token, labels]
     """
-    simplified_rows = []
-    index_offset = -1_000_000
-    for row in tsv_rows:
-        cells = row.split("\t")
+
+    simplified_tokens = []
+    index_offset = None  # For re-indexing to sentence instead of document
+    for row_string in tsv_rows:
+        row = row_string.split("\t")
         # 0-th cell has sent num and token num separated by hyphen
-        sent_num = cells[0][:cells[0].index("-")]
-        token = cells[2]
-        label = cells[4]
-        if label == "_":
-            simplified_rows.append(f"{sent_num}\t{token}\t_")
+        sent_num = row[0][:row[0].index("-")]
+        token = row[2]  # Column 3 is annotation notes, which we don't need
+        label = row[4]
+        empty_label_dict = {"categories": [], "indexes": []}
+        if label == "_":  # Token with no annotations
+            simplified_tokens.append([sent_num, token, empty_label_dict])
         else:
-            categories, annotation_indexes = process_compound_label(label)
-            if index_offset == -1_000_000:
+            categories, annotation_indexes = split_compound_label(label)
+            if index_offset is None:
                 index_offset = annotation_indexes[0] - 1
-            new_label = ""
+            label_dict = {"categories": [], "indexes": []}
             for category, index, in zip(categories, annotation_indexes):
+                label_dict["categories"].append(category)
                 if index == -1:
-                    new_label += f"{category}:{index}|"
+                    label_dict["indexes"].append(index)
                 else:
-                    # New label has a colon to separate categories from indexes and a pipe between each sublabel
-                    new_label += f"{category}:{index-index_offset}|"
-            # Remove extra pipe at end of new_label
-            simplified_rows.append(f"{sent_num}\t{token}\t{new_label[:-1]}")
-    return simplified_rows
+                    label_dict["indexes"].append(index - index_offset)
+            simplified_tokens.append([sent_num, token, label_dict])
+    return simplified_tokens
 
 
 def process_opinion_file(filepath: str) -> list:
@@ -95,57 +106,72 @@ def process_opinion_file(filepath: str) -> list:
     # Make sure sentences start in expected place
     assert data[4].startswith("#Text")
 
-    doc_rows = []
-    sent_rows = []
+    opinion = []  # This will be a list of sentences, which are lists of tokens
+    sentence_rows = []
     for row in data[4:]:
-        # Start of a new sentence, but we don't need to do anything with this line
+        # This indicates the start of a new sentence,
+        # but we don't need to do anything with this line
         if row.startswith("#Text"):
-            sent_rows = []
+            sentence_rows = []
 
         # End of sentence reached
         elif row == "\n":
-            simplified_rows = process_sentence(sent_rows)
-            doc_rows.append(simplified_rows)
+            simplified_tokens = process_sentence(sentence_rows)
+            opinion.append(simplified_tokens)
 
         # Token row
         else:
-            sent_rows.append(row)
+            sentence_rows.append(row)
 
-    doc = [[token.split("\t") for token in sent] for sent in doc_rows]
-
-    return doc
+    return opinion
 
 
-def get_annotations(sentence: list, annotation_column: int) -> Tuple[list, list]:
-    # This is the final product, containing [category, start, stop] entries
+def get_sentence_annotations(sentence: list, annotation_column: int) -> list[list]:
+    """Gets the annotations in a sentence.
+
+    Each annotation is represented as a list:
+    [category, start_token_index, stop_token_index]
+
+    Args:
+        sentence: a list of simplified token rows from process_sentence.
+        annotation_column: Michael's annotations are in column 2
+          and Nathan's are in column 3.
+
+    Returns:
+        A list of lists, where each sublist contains the category of the
+        annotation, the start token index for the annotation, and the end token
+        index for the annotation.
+    """
     annotations = []
 
-    # For keeping track of multi-token annotations
+    # Annotations already have indexes indicating they are the i-th annotation
+    # in a sentence. Here, we're adding start and stop indexes to indicate
+    # which tokens each annotation covers.
     indexed_annotations = {}
     for i, token in enumerate(sentence):
-        label = token[annotation_column]
-        if "*" in label:
+        label_dict = token[annotation_column]
+        if "*" in label_dict["categories"]:
             # These aren't correct; investigate
             print(token)
             break
-        if label == "_":  # No annotation
+        if len(label_dict["categories"]) == 0:  # No annotation
             continue
-        elif "-1" in label:  # "-1" means it's a single-token annotation (e.g. "Metalinguistic Cue:-1")
-            category = label[:label.index(":")]
+        # Single-token annotations
+        elif len(label_dict["categories"]) == 1 and label_dict["indexes"][0] == -1:
+            category = label_dict["categories"][0]
             annotations.append([category, i, i])
         else:
-            token_annotations = label.split("|")
-            for annotation in token_annotations:
-                category, index = annotation.split(":")
-                index = int(index)
-
-                # Add annotation to list of indexed annotations with start and stop indexes
-                if annotation not in indexed_annotations.keys():
-                    indexed_annotations[annotation] = [category, i, i]
-
+            # Identify annotations by category and index in the sentence
+            for category, index in zip(label_dict["categories"],
+                                       label_dict["indexes"]):
+                unique_annotation = f"{category}:{index}"
+                # Add annotation to list of indexed annotations with start and
+                # stop indexes
+                if unique_annotation not in indexed_annotations.keys():
+                    indexed_annotations[unique_annotation] = [category, i, i]
                 # Update the end index the next time we see this annotation
                 else:
-                    indexed_annotations[annotation][2] = i
-    for value in indexed_annotations.values():
-        annotations.append(value)
+                    indexed_annotations[unique_annotation][2] = i
+    for indexed_annotation in indexed_annotations.values():
+        annotations.append(indexed_annotation)
     return annotations
