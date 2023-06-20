12fbef694d800fe859df22f8a5cf3f5137d0a474
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2023-06-20 04:35:24 -0400

    Category constants

---
+++ src/curiam/categories.py
@@ -0,0 +1,51 @@
+CATEGORIES = [
+    "Appeal to Meaning",
+    "Definition",
+    "Direct Quote",
+    "Example Use",
+    "Focal Term",
+    "Language Source",
+    "Legal Source",
+    "Metalinguistic Cue",
+    "Named Interpretive Rule"
+    ]
+
+CATEGORIES_TO_ABBREVIATIONS = {
+    "Appeal to Meaning": "ATM",
+    "Definition": "D",
+    "Direct Quote": "DQ",
+    "Example Use": "EU",
+    "Focal Term": "FT",
+    "Language Source": "LaS",
+    "Legal Source": "LeS",
+    "Metalinguistic Cue": "MC",
+    "Named Interpretive Rule": "NIR"
+}
+
+ABBREVIATIONS_TO_CATEGORIES = {
+    "ATM": "Appeal to Meaning",
+    "D": "Definition",
+    "DQ": "Direct Quote",
+    "EU": "Example Use",
+    "FT": "Focal Term",
+    "LaS": "Language Source",
+    "LeS": "Legal Source",
+    "MC": "Metalinguistic Cue",
+    "NIR": "Named Interpretive Rule",
+}
+
+# TODO: confirm category order in sec 3 matches this
+# Categories ordered how they're presented in the paper
+ORDERED_ABBREVIATIONS = ["FT", "D", "MC", "DQ", "LaS", "LeS", "NIR", "EU", "ATM"]
+
+ORDERED_CATEGORIES = [
+    "Focal Term",
+    "Definition",
+    "Metalinguistic Cue",
+    "Direct Quote",
+    "Language Source",
+    "Legal Source",
+    "Named Interpretive Rule",
+    "Example Use",
+    "Appeal to Meaning",
+]
