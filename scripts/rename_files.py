d1c66006727f0dc2b877997e223df35b12e1f65d
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2023-06-28 03:59:07 -0400

    updated data path

--- scripts/rename_files.py
+++ scripts/rename_files.py
@@ -5,18 +5,18 @@ import shutil # noqa401
 
 from curiam.preprocessing import cap_parsing
 
-for filename in os.listdir("data/full_scale/raw/inception_files"):
+for filename in os.listdir("data/main/raw/inception_files"):
     if filename.endswith(".txt"):
         # Case ID is everything before first underscore
         first_underscore_index = filename.index("_")
         case_id = filename[:first_underscore_index]
         docket_number = cap_parsing.get_docket_number_from_id(case_id)
-        new_folder = "data/full_scale/processed/inception_files_with_docket_numbers"
+        new_folder = "data/main/processed/inception_files_with_docket_numbers"
         os.makedirs(new_folder, exist_ok=True)
         new_file_name = f"{docket_number}_{filename[first_underscore_index+1:]}"
         # Commented out so this doesn't get run and require fixing renamed files again.
         # See comment below.
-        # shutil.copyfile(f"data/full_scale/raw/inception_files/{filename}",
+        # shutil.copyfile(f"data/main/raw/inception_files/{filename}",
         #                f"{new_folder}/{new_file_name}")
 
 # After creating these files, manually rename "mixed" and those lacking opinon type and author names.
