290158949d2b59832ff0c7c6c077a63a8fdbe6bc
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2023-06-28 03:57:57 -0400

    clarified reason for this script's existence

--- scripts/extract_files_from_annotation_folders.py
+++ scripts/extract_files_from_annotation_folders.py
@@ -1,24 +1,29 @@
-"""Copies files in each annotation folder into one folder.
+"""Copies files from each annotation folder into one folder.
 
 INCEpTION puts the annotations for each doument into a document-specific folder.
 
 This script takes a source folder (the folder with all the document-specific
-folders) and copies all of the TSV files out.
+folders) and copies out all of the TSV files for the admin user.
 
+Scenario:
+- Import 'somefile.txt' into INCEpTION
+- Users 'bob' and 'alice' annotate the document.
+- Upon export (with TSV format), a folder named 'somefile.txt' is created.
+- This folder contains 'bob.tsv' and 'alice.tsv'
+
+In this case, annotations are done under the admin account, so this script 
+copies all of the 'admin.tsv' annotation files from each opinion and gives them
+more specific filenames.
 """
 
 import shutil
 
-
 from pathlib import Path
 
-annotation_export_dir = Path("data/full_scale/annotated/inception_export_06_19_23")
+annotation_export_dir = Path("data/main/annotated/inception_export_06_19_23")
 dirs = [x for x in annotation_export_dir.iterdir() if x.is_dir()]
-
 
 for dir in dirs:
     if Path.exists(dir.joinpath("admin.tsv")):
-        # Folders are named after the file that was imported to INCEpTION
-        # so they will have .txt suffixes
-        # Take just the stem of the folder name and add .tsv suffix
-        shutil.copy(dir.joinpath("admin.tsv"), Path("data", "full_scale", "annotated", dir.stem + ".tsv"))
+        shutil.copy(src=dir.joinpath("admin.tsv"),
+                    dst=Path(f"data/main/annotated/{dir.stem}.tsv"))
