a6a8a0b46950bdc4d1a4df0b2f4c427950dda57e
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2023-08-04 23:34:00 -0400

    Added serialization

---
+++ scripts/save_corpus_to_json.py
@@ -0,0 +1,25 @@
+"""Exports the corpus to corpus/corpus.json.
+
+Usage: `python scripts/save_corpus_to_json.py`
+"""
+import json
+
+from pathlib import Path
+
+from curiam.preprocessing import inception_tsv
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if hasattr(obj, "to_json"):
+            return obj.to_json()
+        else:
+            return json.JSONEncoder.default(self, obj)
+
+
+# Serialization: https://stackoverflow.com/questions/5160077/encoding-nested-python-object-in-json
+opinions_dir = Path("data/main/annotated")
+opinions = [inception_tsv.process_opinion_file(opinion_path, opinion_path.name)
+            for opinion_path in sorted(opinions_dir.glob("*.tsv"), key=lambda path: path.name)]
+with open("corpus/corpus.json", "w", encoding="utf-8") as f:
+    json.dump(opinions, f, cls=JSONEncoder)
--- src/curiam/document.py
+++ src/curiam/document.py
@@ -1,6 +1,6 @@
 """Lightweight data models for tokens, sentences, annotations, and documents."""
 
-from collections import Iterator
+from collections.abc import Iterator
 from dataclasses import dataclass, field
 
 
@@ -13,11 +13,12 @@ class TokenAnnotation:
         id: An int indicating this is the i-th annotation in the sentence. Starts at 0.
           This id is used to distinguish overlapping or neighboring annotations.
           Single-token annotations get an id of -1. DO NOT use ids to count annotations.
-        annotator: A string that identifies who provided the annotation.
     """
-    category: str
     id: int
-    annotator: str = ""
+    category: str
+
+    def to_json(self) -> dict:
+        return dict(id=self.id, category=self.category)
 
 
 @dataclass
@@ -31,13 +32,10 @@ class Annotation:
         category: The category/label of the annotation.
         start: The index of the first token covered by the annotation.
         end: The index of the last token covered by the annotation.
-        annotator: A string that identifies who provided the annotation.
     """
-
     category: str
     start: int
     end: int = None
-    annotator: str = ""
 
 
 @dataclass
@@ -49,15 +47,21 @@ class Token:
         id: A zero-indexed int indicating the position of the token in the sentence.
         annotations: `Annotation`s for the token.
     """
-    text: str
     id: int
-    annotations: list[TokenAnnotation] = field(default_factory=list)
+    text: str
+    annotations: list[TokenAnnotation] = None
 
     def get_categories(self) -> set[str]:
         categories = set()
         for token_annotation in self.annotations:
             categories.add(token_annotation.category)
         return categories
+
+    def to_json(self) -> dict:
+        result = dict(id=self.id, text=self.text)
+        if self.annotations is not None:
+            result["annotations"] = self.annotations
+        return result
 
 
 @dataclass
@@ -83,33 +87,32 @@ class Sentence:
     def __str__(self) -> str:
         return str([tok.text for tok in self.tokens])
 
-    def get_annotations(self, annotator: str) -> list[Annotation]:
+    def to_json(self) -> dict:
+        return dict(id=self.id, tokens=self.tokens)
+
+    def get_annotations(self) -> list[Annotation]:
         """Reads `TokenAnnotations` from each token and returns a list of `Annotation`s.
 
         Allows for analyis of annotations at a sentence level.
-
-        Args:
-            annotator: The annotator whose annotations should be retrieved.
         """
 
         annotations = []
         indexed_annotations = {}
         for token in self.tokens:
             for token_annotation in token.annotations:
-                if token_annotation.annotator == annotator:
-                    if token_annotation.id == -1:
-                        annotations.append(Annotation(category=token_annotation.category,
-                                                      start=token.id,
-                                                      end=token.id))
+                if token_annotation.id == -1:  # Single-token annotation
+                    annotations.append(Annotation(category=token_annotation.category,
+                                                  start=token.id,
+                                                  end=token.id))
+                else:
+                    if token_annotation.id not in indexed_annotations.keys():
+                        annotation = Annotation(token_annotation.category,
+                                                start=token.id,
+                                                end=token.id)
+                        indexed_annotations[token_annotation.id] = annotation
                     else:
-                        if token_annotation.id not in indexed_annotations.keys():
-                            annotation = Annotation(token_annotation.category,
-                                                    start=token.id,
-                                                    end=token.id)
-                            indexed_annotations[token_annotation.id] = annotation
-                        else:
-                            # Update the annotation's end index the next time it's seen
-                            indexed_annotations[token_annotation.id].end = token.id
+                        # Update the annotation's end index the next time it's seen
+                        indexed_annotations[token_annotation.id].end = token.id
         for annotation in indexed_annotations.values():
             annotations.append(annotation)
         return annotations
@@ -122,6 +125,7 @@ class Document:
     Attributes:
         sentences: A list of `Sentence`s.
     """
+    name: str
     sentences: list[Sentence] = field(default_factory=list)
 
     def append(self, sentence):
@@ -132,3 +136,6 @@ class Document:
 
     def __len__(self) -> int:
         return len(self.sentences)
+
+    def to_json(self):
+        return dict(name=self.name, sentences=self.sentences)
